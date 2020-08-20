67c119120a84f1d870a1b79b88a20e0631a1de6a
Zcytxcbyz <zcytxcbyz@163.com>
2020-08-20 16:32:21 +0800

    Update

--- Rnpcalc/Rnpcalc.cpp
+++ Rnpcalc/Rnpcalc.cpp
@@ -1,32 +1,58 @@
 #include "RnpCalc.h"
 
 int main() {
-// 	string expr = "8>sin(6-4)?8*15:-tan(-5+4/2.012,float(4))*12.89";
-// 	auto result = retreat(pretreat(expr));
-// 	for (int i = 0; i < result.size(); ++i) {
-// 		for (int j = 0; j < result[i].size(); ++j) {
-// 			cout << result[i][j];
-// 			if (j != result[i].size() - 1) cout << " ";
-// 		}
-// 		cout << endl;
-// 	}
-	string expr = "(18*6)-4/8.23^2";
-	vector<string> result = exprtran(retreat(pretreat(expr))[0]);
-	for (auto iter = result.begin(); iter != result.end(); ++iter) {
-		cout << *iter;
-		if (iter == result.end() - 1) cout << endl;
-		else cout << " ";
-	}
+	string expr;
+	cin >> expr;
+	cout << calculation(expr) << endl;
 	system("pause");
+}
+
+double calculation(string expr) {
+	srand(time(0));
+	vector<double> result;
+	auto rtexpr = retreat(pretreat(expr));	
+	for (int i = 0; i < rtexpr.size(); ++i) {
+		bool iscondition = false;
+		for (int j = 0; j < rtexpr[i].size(); ++j) {
+			if (rtexpr[i][j][0] == '$') {
+				rtexpr[i][j] = to_string(result.at(stoi(rtexpr[i][j].substr(1))));
+			}
+			else if (rtexpr[i][j] == "?") {
+				iscondition = true;
+			}
+		}
+		if (isword(rtexpr[i][0][0])) {
+			function fucexpr = functran(rtexpr[i]);	
+			result.push_back(funccalc(fucexpr));
+		}
+		else if (iscondition) {
+			string temp;
+			if (stoi(rtexpr[i][0])) {
+				temp = rtexpr[i][2];
+			}
+			else {
+				temp = rtexpr[i][4];
+			}
+			result.push_back(stof(temp));
+		}
+		else {
+			vector <string> expexpr = exprtran(rtexpr[i]);
+			result.push_back(exprcalc(expexpr));
+		}
+	}
+	return result.back();
 }
 vector<string> pretreat(string expr) {
 	ReplaceAll(expr, "PI", "3.1415926535898");
 	ReplaceAll(expr, "E", "2.718281828459");
+	ReplaceAll(expr, "rand()", to_string(rand()));
 	for (int i = 0; i < expr.length(); ++i) {
 		if (expr[i] == '-') {
-			if (i != 0 ? !iswordornum(expr[i - 1]) && expr[i - 1] != ')' : true) {
-				expr.insert(i, 1, '0');
-				++i;
+			if (i != 0 ? !isnum(expr[i - 1]) : true)
+			{
+				if (!isnum(expr[i + 1])) {
+					expr.insert(i, 1, '0');
+				}
 			}
 		}
 	}
@@ -99,13 +125,28 @@ vector<vector<string>> retreat(vector<string> exprlist) {
 				}
 				result_item.push_back(string(1, expr[i]));
 			}
+			if (expr[i] == '-') {
+				if (i != 0 ? !iswordornum(expr[i - 1]) && expr[i - 1] != ')' : true) {
+					if (!op_temp.empty()) {
+						result_item.push_back(op_temp);
+						op_temp.clear();
+					}
+					nm_temp.push_back(expr[i]);
+				}
+				else {
+					if (!nm_temp.empty()) {
+						result_item.push_back(nm_temp);
+						nm_temp.clear();
+					}
+					op_temp.push_back(expr[i]);
+				}
+			}
 			else if (isoperand(expr[i]) || expr[i] == '$') {
 				if (!op_temp.empty()) {
 					result_item.push_back(op_temp);
 					op_temp.clear();
 				}
 				nm_temp.push_back(expr[i]);
-
 			}
 			else if (ischaracter(expr[i])) {
 				if (!nm_temp.empty()) {
@@ -139,7 +180,12 @@ function functran(vector<string> expr)
 				temp.push_back(expr[j]);
 			}
 			result.params.push_back(temp);
+			k = i + 1;
+			temp.clear();
 		}
+	}
+	for (auto iter = result.params.begin(); iter != result.params.end(); ++iter) {
+		*iter = exprtran(*iter);
 	}
 	return result;
 }
@@ -156,7 +202,7 @@ vector<string> exprtran(vector<string> expr)
 	stack<string> op_stack;
 	for (int i = 0; i < expr.size(); i++)
 	{
-		if (regex_match(expr[i],regex("-?\\d+(?:\\.\\d+)?")))
+		if (regex_match(expr[i],regex("-?\\d+(?:\\.\\d+)?|\\$\\d+")))
 		{
 			output.push_back(expr[i]);
 		}
@@ -199,4 +245,132 @@ vector<string> exprtran(vector<string> expr)
 		op_stack.pop();
 	}
 	return output;
+}
+
+double exprcalc(vector<string> expr)
+{
+	stack<double> t_stack;
+	for (int i = 0; i < expr.size(); i++)
+	{
+		if (regex_match(expr[i], regex("-?\\d+(?:\\.\\d+)?")))
+		{
+			t_stack.push(atof(expr[i].c_str()));
+		}
+		else
+		{
+			double a = t_stack.top();
+			t_stack.pop();
+			double b = 0;
+			if (expr[i] != "!" && expr[i] != "~") {
+				b = t_stack.top();
+				t_stack.pop();
+			}
+			if (expr[i] == "+")t_stack.push(b + a);
+			else if (expr[i] == "-")t_stack.push(b - a);
+			else if (expr[i] == "*")t_stack.push(b * a);
+			else if (expr[i] == "/")t_stack.push(b / a);
+			else if (expr[i] == "%")t_stack.push((int)b % (int)a);
+			else if (expr[i] == "!")t_stack.push(!a);
+			else if (expr[i] == "&&")t_stack.push(b && a);
+			else if (expr[i] == "||")t_stack.push(b || a);
+			else if (expr[i] == "==")t_stack.push(b == a);
+			else if (expr[i] == ">")t_stack.push(b > a);
+			else if (expr[i] == "<")t_stack.push(b < a);
+			else if (expr[i] == ">=")t_stack.push(b >= a);
+			else if (expr[i] == "<=")t_stack.push(b <= a);
+			else if (expr[i] == "!=")t_stack.push(b != a);
+			else if (expr[i] == "&")t_stack.push((int)b & (int)a);
+			else if (expr[i] == "|")t_stack.push((int)b | (int)a);
+			else if (expr[i] == "^")t_stack.push((int)b ^ (int)a);
+			else if (expr[i] == "~")t_stack.push(~(int)a);
+			else if (expr[i] == "<<")t_stack.push((int)b << (int)a);
+			else if (expr[i] == ">>")t_stack.push((int)b >> (int)a);
+		}
+	}
+	return t_stack.top();
+}
+
+double funccalc(function expr) {
+	vector<double> params;
+	for (vector<string> i : expr.params) {
+		params.push_back(exprcalc(i));
+	}
+	if (expr.name == "max") return max(params[0], params[1]);
+	if (expr.name == "min") return min(params[0], params[1]);
+	if (expr.name == "rand") return rand();
+	if (expr.name == "randint") return randint((int)params[0], (int)params[1]);
+	if (expr.name == "abs") return abs(params[0]);
+	if (expr.name == "cos") return cos(params[0]);
+	if (expr.name == "sin") return sin(params[0]);
+	if (expr.name == "tan") return tan(params[0]);
+	if (expr.name == "acos") return acos(params[0]);
+	if (expr.name == "asin") return asin(params[0]);
+	if (expr.name == "atan") return atan(params[0]);
+	if (expr.name == "atan2") return atan2(params[0], params[1]);
+	if (expr.name == "acosh") return acosh(params[0]);
+	if (expr.name == "asinh") return asinh(params[0]);
+	if (expr.name == "atanh") return atanh(params[0]);
+	if (expr.name == "exp") return exp(params[0]);
+	if (expr.name == "frexp") return frexp(params[0], (int*)&params[1]);
+	if (expr.name == "ldexp") return ldexp(params[0], (int)params[1]);
+	if (expr.name == "log") return log(params[0]);
+	if (expr.name == "log10") return log10(params[0]);
+	if (expr.name == "modf") return modf(params[0], &params[1]);
+	if (expr.name == "exp2") return exp2(params[0]);
+	if (expr.name == "expm1") return expm1(params[0]);
+	if (expr.name == "ilogb") return ilogb(params[0]);
+	if (expr.name == "log1p") return log1p(params[0]);
+	if (expr.name == "log2") return log2(params[0]);
+	if (expr.name == "logb") return logb(params[0]);
+	if (expr.name == "logab") return log(params[1]) / log(params[0]);
+	if (expr.name == "scalbn") return scalbn(params[0], (int)params[1]);
+	if (expr.name == "scalbln") return scalbln(params[0], (long)params[1]);
+	if (expr.name == "pow") return pow(params[0], params[1]);
+	if (expr.name == "sqrt") return sqrt(params[0]);
+	if (expr.name == "cbrt") return cbrt(params[0]);
+	if (expr.name == "hypot") return hypot(params[0], params[1]);
+	if (expr.name == "erf") return erf(params[0]);
+	if (expr.name == "erfc") return erfc(params[0]);
+	if (expr.name == "tgamma") return tgamma(params[0]);
+	if (expr.name == "lgamma") return lgamma(params[0]);
+	if (expr.name == "ceil") return ceil(params[0]);
+	if (expr.name == "floor") return floor(params[0]);
+	if (expr.name == "fmod") return fmod(params[0], params[1]);
+	if (expr.name == "trunc") return trunc(params[0]);
+	if (expr.name == "round") return round(params[0]);
+	if (expr.name == "lround") return lround(params[0]);
+	if (expr.name == "llround") return llround(params[0]);
+	if (expr.name == "rint") return rint(params[0]);
+	if (expr.name == "lrint") return lrint(params[0]);
+	if (expr.name == "llrint") return llrint(params[0]);
+	if (expr.name == "nearbyint") return nearbyint(params[0]);
+	if (expr.name == "remainder") return remainder(params[0], params[1]);
+	if (expr.name == "remquo") return remquo(params[0], params[1], (int*)&params[2]);
+	if (expr.name == "copysign") return copysign(params[0], params[1]);
+	if (expr.name == "nan") {
+		char* str = (char*)malloc(INT16_MAX * sizeof(char));
+		sprintf_s(str, INT16_MAX, "%lf", params[0]);
+		double result = nan(str);
+		free(str);
+		return result;
+	}
+	if (expr.name == "nextafter") return nextafter(params[0], params[1]);
+	if (expr.name == "nexttoward") return nexttoward(params[0], params[1]);
+	if(expr.name=="fdim") return fdim(params[0], params[1]);
+	if (expr.name == "fmax") return fmax(params[0], params[1]);
+	if (expr.name == "fmin") return fmin(params[0], params[1]);
+	if (expr.name == "fabs") return fabs(params[0]);
+	if (expr.name == "fma") return fma(params[0], params[1], params[2]);
+	if (expr.name == "fpclassify") return fpclassify(params[0]);
+	if (expr.name == "isfinite") return isfinite(params[0]);
+	if (expr.name == "isinf") return isinf(params[0]);
+	if (expr.name == "isnan") return isnan(params[0]);
+	if (expr.name == "isnormal") return isnormal(params[0]);
+	if (expr.name == "signbit") return signbit(params[0]);
+	if (expr.name == "isgreater") return isgreater(params[0], params[1]);
+	if (expr.name == "isgreaterequal") return isgreaterequal(params[0], params[1]);
+	if (expr.name == "isless") return isless(params[0], params[1]);
+	if (expr.name == "islessequal") return islessequal(params[0], params[1]);
+	if (expr.name == "islessgreater") return islessgreater(params[0], params[1]);
+	if (expr.name == "isunordered") return isunordered(params[0], params[1]);
 }
--- Rnpcalc/Rnpcalc.h
+++ Rnpcalc/Rnpcalc.h
@@ -5,6 +5,7 @@
 #include <stack>
 #include <string>
 #include <vector>
+#include <ctime>
 #define isword(str) ((str)>='A'&&(str)<='Z'||(str)>='a'&&(str)<='z')
 #define isnum(str) ((str)>='0'&&(str)<='9')
 #define iswordornum(str) (isword(str)||isnum(str))
@@ -12,6 +13,7 @@
 #define isbrackets(str) ((str)=='('||(str)==')')
 #define ischaracter(str) ((!isnum(str)&&str!='.')&&!isbrackets(str))
 #define isoperand(str) ((isnum(str)||str=='.')&&!isbrackets(str))
+#define randint(minval,maxval) ((rand() % (maxval-minval+1))+ minval)
 using namespace std;
 
 map<string, double> Constants =
@@ -28,24 +30,20 @@ map<const string, const int> Symbols =
 	{"?",-1},{":",-1}
 
 };
-map<const string, const int> Functions = {
-	{"negate",1},{"abs",1},{"floor",1},{"ceil",1},{"round",1},
-	{"pow",2},{"sqrt",1},{"log",2},{"lg",1},{"ln",1},
-	{"exp",1},{"rand",0},{"randint",2},{"sin",1},{"cos",1},{"tan",1},
-	{"asin",1},{"acos",1},{"atan",1},{"atan2",1},
-	{"sinh",1},{"cosh",1},{"tanh",1},{"dec",1},{"rad",1},{"max",2},{"min",2}
-};
 
 struct function
 {
 	string name;
 	vector<vector<string>> params;
 };
+double calculation(string expr);
 vector<string> pretreat(string expr);
 vector<vector<string>> retreat(vector<string> exprlist);
 function functran(vector<string> expr);
 int getpriority(string str);
 vector<string> exprtran(vector<string> expr);
+double exprcalc(vector<string>expr);
+double funccalc(function expr);
 void ReplaceAll(string& str,string sub_str,string new_str) {
 	while (str.find(sub_str)!=string::npos)
 	{
